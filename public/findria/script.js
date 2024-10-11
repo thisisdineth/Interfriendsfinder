5e06f8f18a6757596c33d2ab9a5aec6396dd7c99
Dineth Dilshan Gunawardana <infodinethdil@gmail.com>
2024-10-11 20:48:11 +0530

    spelling

--- public/findria/script.js
+++ public/findria/script.js
@@ -92,7 +92,7 @@ const generateAPIResponse = async (incomingMessageDiv) => {
 // Show a loading animation while waiting for the API response
 const showLoadingAnimation = () => {
   const html = `<div class="message-content">
-                  <img class="avatar" src="images/findria.gif" alt="Gemini avatar">
+                  <img class="avatar" src="images/findria.gif" alt="Findria avatar">
                   <p class="text"></p>
                   <div class="loading-indicator">
                     <div class="loading-bar"></div>
