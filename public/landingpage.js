9102bb12ea9916204ca7912d2a01a1c69d0dc1db
Dineth Dilshan Gunawardana <infodinethdil@gmail.com>
2024-10-22 21:34:45 +0530

    #

--- public/landingpage.js
+++ public/landingpage.js
@@ -4,3 +4,8 @@ window.addEventListener('load', function() {
         document.getElementById('loading-screen').style.display = 'none';
     }, 1500); // 1.5 seconds loading screen
 });
+
+// Toggle navigation menu in mobile view
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('nav ul').classList.toggle('active');
+});
